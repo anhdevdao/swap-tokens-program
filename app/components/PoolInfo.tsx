af60e113d0dceb82cc266419aa2ae0bf73a01a16
AnhDao <tuananh2998@gmail.com>
2023-05-05 22:45:33 +0700

    feat: update NOTE message

--- app/components/PoolInfo.tsx
+++ app/components/PoolInfo.tsx
@@ -10,13 +10,16 @@ export const PoolInfo = ({poolSolBalance, poolMoveBalance}: Props): React.ReactE
 
   return (
     <Text style={{color: 'white'}}>
+      <p>
+        <strong>NOTE!!! Reload page if value of balance do not change after sending transaction</strong>
+      </p>
       <span>
         <strong>Private Key with Prefund Tokens: </strong>
         <p>
-        52ruwo5CT8C1eZXjHPHMKYPDSUMeuwUvq4zL45Fn2WZfkF4mnn6kWaFDA8Z75nYPwhTjRkEUshwsKh75V5nRqd55
+          52ruwo5CT8C1eZXjHPHMKYPDSUMeuwUvq4zL45Fn2WZfkF4mnn6kWaFDA8Z75nYPwhTjRkEUshwsKh75V5nRqd55
         </p>
         <p>
-        mHY2aCChJGbNSdtgfEuvriAQ8FkoxWeX4XwdZRgnrJYy9RzGfyjWvWK3wZzrKThHY3FN1EmRsqDHjygpxzoea8D
+          mHY2aCChJGbNSdtgfEuvriAQ8FkoxWeX4XwdZRgnrJYy9RzGfyjWvWK3wZzrKThHY3FN1EmRsqDHjygpxzoea8D
         </p>
         <strong>Pool SOL Balance: </strong> { poolSolBalance } SOL {" "}
         <strong>Pool MOVE Balance: </strong> { poolMoveBalance} MOVE
